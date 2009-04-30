9997ed4b0f8e93f3f207361f9f5fd389de67231a
taf2 <taf2@3c018e3d-4fa3-4659-95e0-694c3f9c3de5>
2009-04-30 19:03:37 +0000

    add a logo for a default theme themes can have a manifest file to describe them
    git-svn-id: https://web2.anerian.com/repos/anerian/railspress@5875 3c018e3d-4fa3-4659-95e0-694c3f9c3de5

--- app/controllers/admin/themes_controller.rb
+++ app/controllers/admin/themes_controller.rb
@@ -1,6 +1,23 @@
 class Admin::ThemesController < Admin::AdminController
   def index
-    @themes = Dir["#{THEME_ROOT}/*"].reject{|t| File.basename(t) == 'active' }.map{|t| [File.basename(t),t]}
+    @themes = Dir["#{THEME_ROOT}/*"].reject{|t| File.basename(t) == 'active' or !File.exist?("#{THEME_ROOT}/#{File.basename(t)}/manifest.yml") }
+    @themes.map!{|t|
+      begin
+        {
+          :name => File.basename(t),
+          :path => t,
+          :url_to_theme => '/' + Pathname.new(t).relative_path_from(Pathname.new(File.join(RAILS_ROOT,'public'))),
+          :details =>YAML.load_file("#{THEME_ROOT}/#{File.basename(t)}/manifest.yml")
+        } 
+      rescue => e
+        {
+          :name => File.basename(t),
+          :path => t,
+          :error => true,
+          :message => e.message
+        } 
+      end
+    }
   end
 
   def widgets
