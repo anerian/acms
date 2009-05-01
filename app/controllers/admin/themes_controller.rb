01d62255793ad46341cd8e5bdfe6cb03bf4b55bf
taf2 <taf2@3c018e3d-4fa3-4659-95e0-694c3f9c3de5>
2009-05-01 13:06:06 +0000

    more theme updates
    git-svn-id: https://web2.anerian.com/repos/anerian/railspress@5882 3c018e3d-4fa3-4659-95e0-694c3f9c3de5

--- app/controllers/admin/themes_controller.rb
+++ app/controllers/admin/themes_controller.rb
@@ -1,14 +1,20 @@
 class Admin::ThemesController < Admin::AdminController
   def index
     @themes = Dir["#{THEME_ROOT}/*"].reject{|t| File.basename(t) == 'active' or !File.exist?("#{THEME_ROOT}/#{File.basename(t)}/manifest.yml") }
+    @active = nil
     @themes.map!{|t|
       begin
-        {
+        theme = {
           :name => File.basename(t),
           :path => t,
           :url_to_theme => '/' + Pathname.new(t).relative_path_from(Pathname.new(File.join(RAILS_ROOT,'public'))),
           :details =>YAML.load_file("#{THEME_ROOT}/#{File.basename(t)}/manifest.yml")
-        } 
+        }
+        if Pathname.new(File.expand_path(t)).realpath == Pathname.new(File.expand_path(THEME_PATH)).realpath
+          theme[:active] = true
+          @active = theme
+        end
+        theme
       rescue => e
         {
           :name => File.basename(t),
