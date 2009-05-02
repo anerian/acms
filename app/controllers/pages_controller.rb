fc3ac94a6cad652ab22db6ebacdc93b90ea32c75
taf2 <taf2@3c018e3d-4fa3-4659-95e0-694c3f9c3de5>
2009-05-02 16:41:04 +0000

    simplify pages controller
    git-svn-id: https://web2.anerian.com/repos/anerian/railspress@5891 3c018e3d-4fa3-4659-95e0-694c3f9c3de5

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,20 +1,24 @@
 class PagesController < ApplicationController
+  before_filter :load_site_info
 
   def index
+    @page = params[:page]
+    @page = 1 if @page.blank?
+    @pages = Page.paginate(:all, {:page => @page})
+    render :layout => 'default'
   end
 
   def show
-    # lookup the page for this request
-    @page = Page.find_by_id(params[:id])
-    @site_info = Option.load_by_key('site_info')
-    if @page
+    if params[:id]
+      @page = Page.find_by_id(params[:id])
       render :layout => 'default'
     else
-      @page = params[:page]
-      @page = 1 if @page.blank?
-      @pages = Page.paginate(:all, {:page => @page})
       render :action => 'index', :layout => 'default'
     end
   end
 
+protected
+  def load_site_info
+    @site_info = Option.load_by_key('site_info')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ ActionController::Routing::Routes.draw do |map|
   # The priority is based upon order of creation: first created -> highest priority.
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  map.root :controller => 'pages', :action => 'show'
+  map.root :controller => 'pages', :action => 'index'
   map.resources :pages, :only => [:index, :show]
 
   # named user routes
