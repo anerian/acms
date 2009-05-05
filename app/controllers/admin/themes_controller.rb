1e28785bdd39bae95c3faa5c7f2a14fe224d61f2
taf2 <taf2@3c018e3d-4fa3-4659-95e0-694c3f9c3de5>
2009-05-05 01:33:28 +0000

    fix theme switching
    git-svn-id: https://web2.anerian.com/repos/anerian/railspress@5920 3c018e3d-4fa3-4659-95e0-694c3f9c3de5

--- app/controllers/admin/themes_controller.rb
+++ app/controllers/admin/themes_controller.rb
@@ -8,7 +8,13 @@ class Admin::ThemesController < Admin::AdminController
   def activate
     theme = params[:theme]
     if theme and (theme=Theme.find_by_name(theme))
-      theme.activate!
+      THEME_PREVIEW_MUTEX.synchronize do # XXX: view_paths is not request scoped must lock
+        theme.activate!
+        ActionController::Base.view_paths.shift
+        paths = ActionView::PathSet.new([File.join(theme.path,'views')] + ActionController::Base.view_paths )
+        ActionController::Base.view_paths = paths
+        view_paths = paths
+      end
       render :status => 200, :text => "activated" and return
     end
   end
